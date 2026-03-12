c7ba0b0b505e974ee0405e75c4d3a73f2507c2b8
YaelK17 <46296@guttman.ort.org.il>
2026-03-12 12:47:09 +0200

    added color change on hover in answer Questions page

--- src/pages/playerDisplay/AnswerOptions.tsx
+++ src/pages/playerDisplay/AnswerOptions.tsx
@@ -8,12 +8,12 @@ import PlayerCard from '../../components/player/playerCard';
 const AnswerOptions = () => {
     const answersCount = 4; // to change to receive from server later
     const options = [
-        { color: "#1368CE", shape: <Diamond /> },
-        { color: "#E21B3C", shape: <Triangle /> },
-        { color: "#26890C", shape: <Square /> },
-        { color: "#D89E00", shape: <Circle /> },
-        { color: "#0AA3A3", shape: <Diamond /> },
-        { color: "#864CBF", shape: <Diamond /> }
+        { color: "#1368CE", shape: <Diamond />, colorOnHover: "#0057BA" },
+        { color: "#E21B3C", shape: <Triangle />, colorOnHover: "#CB002C" },
+        { color: "#26890C", shape: <Square />, colorOnHover: "#007600" },
+        { color: "#D89E00", shape: <Circle />, colorOnHover: "#C28B00" },
+        { color: "#0AA3A3", shape: <Diamond />, colorOnHover: "#099494ff" },
+        { color: "#864CBF", shape: <Diamond />, colorOnHover: "#7845acff" }
     ];
     return (
         <>
@@ -22,7 +22,12 @@ const AnswerOptions = () => {
                     <div
                         key={i}
                         className={classes["answer-option"]}
-                        style={{ backgroundColor: options[i].color }}
+                        style={
+                            {
+                                "--bg-color": options[i].color,
+                                "--hover-color": options[i].colorOnHover
+                            } as React.CSSProperties
+                        }
                     >
                         {options[i].shape}
                     </div>
