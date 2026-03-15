3fc81e22b17e73c308172148b403763bb9a343e1
YaelK17 <46296@guttman.ort.org.il>
2026-03-15 16:28:15 +0200

    added answerFeedback

---
+++ src/components/player/AnswerFeedback.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import correctSymbol from '../../assets/correct.svg';
+import mistakeSymbol from '../../assets/mistake.svg';
+import classes from './AnswerFeedback.module.css';
+
+const AnswerFeedback: React.FC<{ wasCorrect: boolean }> = (props) => {
+    const title = props.wasCorrect ? 'תשובה נכונה' : 'תשובה לא נכונה';
+    const symbol = props.wasCorrect ? correctSymbol : mistakeSymbol;
+    const divClass = props.wasCorrect ? classes.correct : classes.mistaken;
+
+    return (
+        <div className={classes.centering}>
+            <p className={classes.title}>{title}</p>
+            <div className={divClass}>
+                <img className={classes.symbol} src={symbol} alt={title} />
+            </div>
+            <p className={classes.text}>מקום 3 אחרי מתן גרשון</p>
+            {/* todo change this text to what is received from server */}
+        </div>
+    );
+}
+
+export default AnswerFeedback;
--- src/pages/playerDisplay/PlayerGamePage.tsx
+++ src/pages/playerDisplay/PlayerGamePage.tsx
@@ -2,6 +2,7 @@ import PlayerCard from '../../components/player/playerCard';
 import AnswerOptions from '../../components/player/AnswerOptions';
 import { useState } from 'react';
 import Loading from '../../components/player/Loading';
+import AnswerFeedback from '../../components/player/AnswerFeedback';
 
 const PlayerGamePage = () => {
     const [status, setStatus] = useState("answering");
@@ -14,8 +15,8 @@ const PlayerGamePage = () => {
             { status === "answering" && <AnswerOptions answersCount={4} onAnswerClick={handleAnswerClick} /> } 
             {/* to change answersCount number to receive from server later */}
             { status === "loading" && <Loading /> }
-            { status === "correct" && <Loading /> }
-            { status === "wrong" && <Loading /> }
+            { status === "correct" && <AnswerFeedback wasCorrect={true} /> }
+            { status === "wrong" && <AnswerFeedback wasCorrect={false} /> }
             <PlayerCard name='יעל' points='777' />
         </>
     )
