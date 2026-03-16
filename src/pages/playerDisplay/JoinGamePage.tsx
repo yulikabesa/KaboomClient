dcb0980d0d1bed4b616f711a958741ef3bf8c7c1
YaelK17 <46296@guttman.ort.org.il>
2026-03-16 11:26:05 +0200

    added socket connection to join game and nickname input

--- src/pages/playerDisplay/JoinGamePage.tsx
+++ src/pages/playerDisplay/JoinGamePage.tsx
@@ -2,24 +2,58 @@ import classes from './JoinGamePage.module.css';
 import kaboomLogo from '../../assets/kaboomLogo.png';
 import React, { useState } from 'react';
 import { MyKabooms } from '../projectorDisplay/MyKabooms';
+import { connectSocket } from "../../services/socketService";
 
 const JoinGamePage: React.FC = () => {
     const [pin, setPin] = useState('');
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const [didSubmitPin, setDidSubmitPin] = useState(false);
+    const [nickname, setNickname] = useState('');
+
+    const handlePinChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setPin(event.target.value);
     };
+
+    const handleNicknameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNickname(event.target.value);
+    };
+
+    const joinGame = () => {
+        const socket = connectSocket();
+
+        // Emit event to join game
+        socket.emit("join-game", { pin, nickname });
+        // todo navigate to game page
+
+
+        // todo move this to project side
+        // Listen for the game-created event only once
+        socket.once("player-joined", ({ player }: { player: { id: string; nickname: string; } }) => {
+            console.log("player joined:", player);
+        });
+    };
+
     return (
         <>
             <div className={classes['parent-div']}>
                 <img src={kaboomLogo}
                     alt="kaboom logo" />
                 <div className={classes['child-div']}>
-                    <input type="text"
-                        value={pin}
-                        placeholder='הכנס קוד'
-                        onChange={handleChange}
-                        className={classes['pin-input']} />
-                    <button className={classes['join-button']}>כנס</button>
+                    {!didSubmitPin ? (<>
+                        <input type="text"
+                            value={pin}
+                            placeholder='הכנס קוד'
+                            onChange={handlePinChange}
+                            className={classes['pin-input']} />
+                        <button className={classes['join-button']} onClick={() => { setDidSubmitPin(true); }}>כנס</button>
+                    </>) : (<>
+                        <input type="text"
+                            value={nickname}
+                            placeholder='כתוב שם'
+                            onChange={handleNicknameChange}
+                            className={classes['pin-input']} />
+                        <button className={classes['join-button']} onClick={joinGame}>אחלה, מתחברים!</button>
+                    </>)
+                    }
                 </div>
             </div>
             <MyKabooms />
