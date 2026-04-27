6d19d4387109b65312906bf723713872ea9bba5e
Yael <yulikabesa@gmail.com~>
2026-04-27 08:57:31 +0300

    made menu fixed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,25 +5,26 @@ import ProductsList from "../components/ProductsList";
 const Home = () => {
   const [products, setProducts] = useState([3, 4, 3, 1, 3, 4, 3, 2]);
   return (
-    <div className={classes.background}>
+    <>
       <NavigationMenu />
-      <div className={classes.container}>
-        <div className={classes["question-marks-div"]}>
-          <p className={classes["tilted-title"]}>נתקעת בלי שיעור?</p>
-          <p className={classes["sub-title"]}>
-            עם עשרות התבניות של קבום – כל אחד יכול להכין שיעור במהירות.
-          </p>
+      <div className={classes.background}>
+        <div className={classes.container}>
+          <div className={classes["question-marks-div"]}>
+            <p className={classes["tilted-title"]}>נתקעת בלי שיעור?</p>
+            <p className={classes["sub-title"]}>
+              עם עשרות התבניות של קבום – כל אחד יכול להכין שיעור במהירות.
+            </p>
+          </div>
+          <p className={classes["sub-title"]}>תוצרים שיצרתי</p>
+          <ProductsList products={[1, 2, 3, 4, 5]} />
+          <p className={classes["sub-title"]}>התוצרים ששותפו איתי</p>
+          <ProductsList products={products} />
+          <p className={classes["sub-title"]}>תוצרים של הקורס שלי</p>
+          <ProductsList products={[1, 2, 5, 5]} />
+          {/* todo change products to receive from server */}
         </div>
-        <p className={classes["sub-title"]}>תוצרים שיצרתי</p>
-        <ProductsList products={[1, 2, 3, 4, 5]} />
-        <p className={classes["sub-title"]}>התוצרים ששותפו איתי</p>
-        <ProductsList products={products} />
-        <p className={classes["sub-title"]}>תוצרים של הקורס שלי</p>
-        <ProductsList products={[1, 2, 5, 5]} />
-        {/* todo change products to receive from server */}
       </div>
-      <div className={classes.space} />
-    </div>
+    </>
   );
 };
 
