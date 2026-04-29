e96b5fba8c13903246e25cfc140d760fe8a26437
Yael <yulikabesa@gmail.com~>
2026-04-29 10:09:19 +0300

    added create page partly

---
+++ src/components/create/QuestionHistory.tsx
@@ -0,0 +1,30 @@
+import classes from "./QuestionHistory.module.css";
+import exampleToChange from "../../assets/kaboomBackground.png";
+import type React from "react";
+
+const QuestionHistory: React.FC<{
+  questionText: string;
+  timeLimit: number;
+  questionImage: string;
+  isCurrentlyEdited: boolean;
+}> = (props) => {
+  return (
+    <div
+      className={classes.container}
+      style={{
+        backgroundColor: props.isCurrentlyEdited ? "#FFFFFF" : "#f2f2f2",
+        border: props.isCurrentlyEdited ? "2px solid #3E6CC4" : "none",
+      }}
+    >
+      <p className={classes.title}>{props.questionText}</p>
+      <div className={classes["middle-items"]}>
+        {props.questionImage !== "" && (
+          <img src={exampleToChange} className={classes["question-image"]} />
+        )}
+        <div className={classes["question-time"]}>{props.timeLimit}</div>
+      </div>
+    </div>
+  );
+};
+
+export default QuestionHistory;
--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,10 +1,102 @@
 import NavigationMenu from "../components/NavigationMenu";
-import classes from './Create.module.css';
+import classes from "./Create.module.css";
+import QuestionHistory from "../components/create/QuestionHistory";
+import { useState, type ChangeEvent } from "react";
+
+type QuestionType = {
+  questionText: string;
+  answerOptions: string[];
+  correctIndexes: number[];
+  timeLimit: number;
+  scoringWeight: 0.5 | 1 | 2;
+  questionImage: string;
+};
 
 const Create = () => {
+  // דוגמא לשאלות
+  const [questions, setQuestions] = useState<QuestionType[]>([
+    {
+      questionText: "מה אתה עושה כשאתה קם בבוקר?",
+      answerOptions: ["this", "that"],
+      correctIndexes: [1],
+      timeLimit: 20,
+      scoringWeight: 1,
+      questionImage: "xx",
+    },
+  ]);
+  const [currentQuestionEdited, setCurrentQuestionEdited] = useState(0);
+  const [questionTextInput, setQuestionTextInput] = useState("");
+  
+  const handleQuestionTextInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setQuestionTextInput(e.target.value);
+    setQuestions((prev)=> {
+        prev[currentQuestionEdited].questionText = e.target.value;
+        return prev;
+    });
+  };
+
+  const addEmptyQuestion = () => {
+    setQuestions((prev) => {
+      return [
+        ...prev,
+        {
+          questionText: "",
+          answerOptions: [],
+          correctIndexes: [],
+          timeLimit: 20,
+          scoringWeight: 1,
+          questionImage: "",
+        },
+      ];
+    });
+    setCurrentQuestionEdited(questions.length);
+  };
   return (
     <div className={classes.background}>
       <NavigationMenu variant="create" />
+      {/* question editing */}
+      <div className={classes["question-editing"]}>
+        <input
+          type="text"
+          placeholder="הקלד כאן את השאלה שלך…"
+          className={classes["question-text-input"]}
+          value={questionTextInput}
+          onChange={handleQuestionTextInputChange}
+        />
+      </div>
+      {/* question navigator */}
+      <div className={classes["question-navigator"]}>
+        {questions.map((question: QuestionType, index) => (
+          <div
+            key={index}
+            style={{
+              width: "100%",
+              color: "#6E6E6E",
+              paddingTop: "2vh",
+              paddingBottom: "2vh",
+              cursor: "pointer",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              backgroundColor:
+                currentQuestionEdited === index ? "#ECF4FB" : "transparent",
+            }}
+            onClick={() => setCurrentQuestionEdited(index)}
+          >
+            {index + 1} שאלה
+            <QuestionHistory
+              key={index}
+              questionImage={question.questionImage}
+              questionText={question.questionText}
+              timeLimit={question.timeLimit}
+              isCurrentlyEdited={currentQuestionEdited === index ? true : false}
+            />
+          </div>
+        ))}
+        <div className={classes["blue-btn"]} onClick={addEmptyQuestion}>
+          הוסף שאלה
+        </div>
+      </div>
     </div>
   );
 };
