ec4cf7c4193455d9908b7a6c724d5c9b127eeb21
arlinka-a <arlin.agarunov@gmail.com>
2026-04-30 15:18:00 +0300

    added overlay component

---
+++ src/components/UI/Overlay.tsx
@@ -0,0 +1,51 @@
+import React, { type PropsWithChildren } from "react";
+import ReactDOM from "react-dom";
+import closeIcon from "../../assets/closeIcon.svg";
+import classes from "./Overlay.module.css";
+
+interface OverlayProps extends PropsWithChildren {
+  className?: string;
+  title: string;
+  button?: boolean;
+  closeOverlay: () => void;
+}
+
+const Backdrop: React.FC<{ onClick: () => void }> = (props) => {
+  return <div className={classes["backdrop"]} onClick={props.onClick} />;
+};
+
+const OverlayCard: React.FC<OverlayProps> = (props) => {
+  return (
+    <div className={classes["card"]}>
+      <header className={classes["header"]}>
+        <p>{props.title}</p>
+        {props.button && (
+          // todo: fix
+          <div className={classes["btn"]} onClick={props.closeOverlay}>
+            {/* <img src={closeIcon} /> */}
+          </div>
+        )}
+      </header>
+      <div className={classes["content"]}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement: HTMLElement = document.getElementById("overlay")!;
+
+const Overlay: React.FC<OverlayProps> = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={props.closeOverlay} />,
+        portalElement,
+      )}
+      {ReactDOM.createPortal(
+        <OverlayCard {...props}>{props.children}</OverlayCard>,
+        portalElement,
+      )}
+    </>
+  );
+};
+
+export default Overlay;
