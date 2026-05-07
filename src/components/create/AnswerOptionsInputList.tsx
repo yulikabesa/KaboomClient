fdae09f0623912762fae1692d6af1bab49aaf62d
Yael <yulikabesa@gmail.com~>
2026-05-07 10:03:50 +0300

    fixed hover

--- src/components/create/AnswerOptionsInputList.tsx
+++ src/components/create/AnswerOptionsInputList.tsx
@@ -63,6 +63,7 @@ const AnswerOptionsInputList: React.FC<Props> = ({
     <div className={classes.container}>
       {Array.from({ length: answersCount }).map((_, i) => {
         const isCorrect = correctAnswerIndexes?.includes(i) || false;
+        const isNotActive = (answerTexts[i] ?? "") === "" && i > 1;
         return (
           <div
             key={i}
@@ -73,17 +74,14 @@ const AnswerOptionsInputList: React.FC<Props> = ({
                 "--hover-color": options[i].colorOnHover,
                 boxShadow: `0 3px 0 0 ${options[i].shadowColor}`,
                 border: `1px solid ${options[i].shadowColor}`,
-                filter:
-                  (answerTexts[i] ?? "") === "" && i > 1
-                    ? "brightness(0.75)"
-                    : "brightness(1)",
+                filter: isNotActive ? "brightness(0.75)" : "brightness(1)",
               } as React.CSSProperties
             }
           >
             <span
-              className={`${classes.resultSign} ${isCorrect && classes.correct}`}
+              className={`${classes.resultSign} ${isCorrect && classes.correct} ${!isNotActive && classes["hover-enabled"]}`}
               onClick={() => {
-                if (!((answerTexts[i] ?? "") === "" && i > 1)) onAnswerClick(i);
+                if (!isNotActive) onAnswerClick(i);
               }}
             />
 
