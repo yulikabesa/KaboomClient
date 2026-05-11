a441f1c457ca9618612ab6d7fb836bcf0f64e613
Yael <yulikabesa@gmail.com~>
2026-05-11 14:27:16 +0300

    split QuestionSlideList to a few components

--- src/components/create/QuestionSlideList.tsx
+++ src/components/create/QuestionSlideList.tsx
@@ -1,124 +1,46 @@
 import React from "react";
-import QuestionSlide from "./QuestionSlide";
-import copyIcon from "../../assets/grayCopyIcon.svg";
-import deleteIcon from "../../assets/grayDeleteIcon.svg";
-import classes from "./QuestionSlideList.module.css";
+import { DragDropContext, Droppable } from "@hello-pangea/dnd";
+import DraggableQuestionItem from "./DraggableQuestionItem";
 import type { questionType } from "../ProductsList";
-import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 
-const QuestionSlideList: React.FC<{
+type Props = {
   questions: questionType[];
   currentQuestionEdited: number;
   onSlideClick: (index: number) => void;
   onSlideCopyClick: () => void;
   onSlideDeleteClick: () => void;
   onDragEnd: (result: any) => void;
-}> = (props) => {
+};
+
+const QuestionSlideList: React.FC<Props> = ({
+  questions,
+  currentQuestionEdited,
+  onSlideClick,
+  onSlideCopyClick,
+  onSlideDeleteClick,
+  onDragEnd,
+}) => {
   return (
-    <DragDropContext onDragEnd={props.onDragEnd}>
+    <DragDropContext onDragEnd={onDragEnd}>
       <Droppable droppableId="questions-list">
         {(provided) => (
           <div
-            style={{ width: "100%" }}
             ref={provided.innerRef}
             {...provided.droppableProps}
+            style={{ width: "100%" }}
           >
-            {props.questions.map((question, index) => (
-              <Draggable
+            {questions.map((question, index) => (
+              <DraggableQuestionItem
                 key={question._id}
-                draggableId={question._id}
+                question={question}
                 index={index}
-                isDragDisabled={props.currentQuestionEdited !== index}
-              >
-                {(provided) => (
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    className={classes.container}
-                    style={{
-                      backgroundColor:
-                        props.currentQuestionEdited === index
-                          ? "#ECF4FB"
-                          : "transparent",
-                      ...provided.draggableProps.style,
-                    }}
-                    onClick={() => props.onSlideClick(index)}
-                  >
-                    <div className={classes["all-items-wrapper"]}>
-                      {/* LEFT SIDE (number + icons) */}
-                      <div className={classes["icons-and-number-container"]}>
-                        <div
-                          className={`${classes["question-num"]} ${classes["center-text"]}`}
-                          style={{
-                            color:
-                              props.currentQuestionEdited === index
-                                ? "black"
-                                : "#6e6e6e",
-                          }}
-                        >
-                          {index + 1}
-                        </div>
-
-                        <div
-                          className={classes["icons-container"]}
-                          style={{
-                            visibility:
-                              props.currentQuestionEdited === index
-                                ? "visible"
-                                : "hidden",
-                          }}
-                        >
-                          <img
-                            className={classes.icon}
-                            src={copyIcon}
-                            onClick={(e) => {
-                              e.stopPropagation();
-                              props.onSlideCopyClick();
-                            }}
-                          />
-
-                          <img
-                            className={classes.icon}
-                            src={deleteIcon}
-                            onClick={(e) => {
-                              e.stopPropagation();
-                              props.onSlideDeleteClick();
-                            }}
-                          />
-                        </div>
-                      </div>
-
-                      {/* RIGHT SIDE */}
-                      <div className={classes["question-and-slide-container"]}>
-                        <div
-                          className={classes["question-num"]}
-                          style={{
-                            color:
-                              props.currentQuestionEdited === index
-                                ? "black"
-                                : "#6e6e6e",
-                          }}
-                        >
-                          שאלה
-                        </div>
-
-                        <QuestionSlide
-                          questionImage={question.questionImage ?? ""}
-                          questionText={question.questionText}
-                          timeLimit={question.timeLimit}
-                          isCurrentlyEdited={
-                            props.currentQuestionEdited === index
-                          }
-                          answersCount={question.answerOptions.length}
-                          correctAnswerIndexes={question.correctIndexes}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                )}
-              </Draggable>
+                isActive={currentQuestionEdited === index}
+                onClick={() => onSlideClick(index)}
+                onCopy={onSlideCopyClick}
+                onDelete={onSlideDeleteClick}
+              />
             ))}
+
             {provided.placeholder}
           </div>
         )}
