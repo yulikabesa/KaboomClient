18d9b1898d276e9b3808aae9afe0e9c2d5a7db87
arlinka-a <arlin.agarunov@gmail.com>
2026-06-14 09:54:16 +0300

    added media lib component

---
+++ src/components/create/Image/MediaLibrary.tsx
@@ -0,0 +1,44 @@
+import Overlay from "../../UI/Overlay";
+import classes from "./MediaLibrary.module.css";
+import img from "../../../assets/defaultCoverPhoto.png";
+import img2 from "../../../assets/questionMarksBackground.png";
+
+const categories: string[] = [
+  "הכל",
+  "תמונות צבאיות",
+  "איסלאם",
+  "הכשרה",
+  "מודיעין שדה",
+  "סייבר",
+  "שטח",
+  "לוחמים",
+  "מנהיגות",
+];
+
+const media: string[] = [img2, img, img, img, img, img2, img, img, img, img2];
+
+const MediaLibrary: React.FC = () => {
+  return (
+    <Overlay
+      title="מאגר תמונות"
+      button={true}
+      closeOverlay={() => {}}
+      elementId="overlay"
+    >
+      <div className={classes["layout"]}>
+        <div className={classes["category-list"]}>
+          {categories.map((category) => (
+            <button className={classes["category-btn"]}>{category}</button>
+          ))}
+        </div>
+        <div className={classes["media-grid"]}>
+          {media.map((image) => (
+            <img className={classes["image"]} src={image} />
+          ))}
+        </div>
+      </div>
+    </Overlay>
+  );
+};
+
+export default MediaLibrary;
