b5ff24390c2c3423eaf68cf609be4faf63f17237
arlinka-a <arlin.agarunov@gmail.com>
2026-06-24 12:46:08 +0300

    made css changes to answer feedback

--- src/components/player/AnswerFeedback.tsx
+++ src/components/player/AnswerFeedback.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import correctSymbol from "../../assets/correct.svg";
-import mistakeSymbol from "../../assets/mistake.svg";
 import classes from "./AnswerFeedback.module.css";
 
 const AnswerFeedback: React.FC<{
@@ -9,14 +7,12 @@ const AnswerFeedback: React.FC<{
   rankAbove: string;
 }> = (props) => {
   const title = props.isCorrect ? "תשובה נכונה" : "תשובה לא נכונה";
-  const symbol = props.isCorrect ? correctSymbol : mistakeSymbol;
-  const divClass = props.isCorrect ? classes.correct : classes.mistaken;
+  const divClass = props.isCorrect ? classes.correct : classes.wrong;
 
   return (
     <div className={classes.centering}>
       <p className={classes.title}>{title}</p>
       <div className={divClass}>
-        <img className={classes.symbol} src={symbol} alt={title} />
       </div>
       {props.currentRank && (
         <p className={classes.text}>
